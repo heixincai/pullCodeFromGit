e0a3f9adfee04501c6cd6b1cdcf9e7b01e0eabb8
SYD <957548804@qq.com>
2019-01-03 20:55:12 +0800

    将所有的代码提交至Git

---
+++ pullCodeFromGit/src/com/company/TestJunit.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class TestJunit {
+    public void print(){
+        System.out.println("hello world");
+    }
+}
---
+++ pullCodeFromGit/test/com/company/TestJunitTest.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import org.junit.Test;
+
+public class TestJunitTest {
+
+    @Test
+    public void print() {
+        new TestJunit().print();
+    }
+}
